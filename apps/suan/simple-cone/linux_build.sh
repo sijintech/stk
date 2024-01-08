79ffdf6e853b560b00eeb6afc3f2af3b497ded5c
周瀚杰 <2625553453@qq.com>
2024-01-08 20:06:48 +0800

    build

---
+++ apps/suan/simple-cone/linux_build.sh
@@ -0,0 +1,26 @@
+# 生成并进入python虚拟环境
+sudo apt install python3.8-venv
+python3 -m venv .venv
+source .venv/bin/activate
+# 安装需要的包
+pip install -U pip
+pip install trame pyinstaller trame_components trame_vuetify trame_vtk
+# 打包生成可执行文件（web）
+python -m PyInstaller \
+    -F --clean --noconfirm \
+    --distpath src-tauri \
+    --name server \
+    --hidden-import pkgutil \
+    --collect-data trame_client \
+    --collect-data trame_components \
+    --collect-data trame_vuetify \
+    --collect-data trame_vtk \
+    server.py
+# 打包生成桌面文件
+sudo apt  install cargo
+sudo cargo install tauri-cli
+cd src-tauri
+cargo tauri icon
+sudo cargo tauri build
+# 打开桌面文件
+open target/release/bundle/macos/Cone.app
