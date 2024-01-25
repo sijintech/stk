f36f859980c2e673b775ceaef2af00e2a4451c3b
周瀚杰 <2625553453@qq.com>
2024-01-25 12:00:53 +0800

    depoly

--- apps/suan/simple-cone/deploy.py
+++ apps/suan/simple-cone/deploy.py
@@ -9,23 +9,24 @@ auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
 bucket = oss2.Bucket(auth, os.environ['OSS_ENDPOINT'], os.environ['OSS_BUCKET'])
 
 # 指定要上传的文件或目录
-source_dir = os.environ['SOURCE_DIR']
+# source_dir = os.environ['SOURCE_DIR']
+this_dir=os.path.dirname(os.path.abspath(__file__))
+source_dir = os.path.join(this_dir, os.environ['SOURCE_DIR'])
 
 # 指定OSS中的目标目录
 target_dir = os.environ['TARGET_DIR']
 print(f"source_dir:{source_dir}")
-this_dir=os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(this_dir, 'server.py')
 
 # 必须以二进制的方式打开文件。
 # 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
-with open(file_path, 'rb') as fileobj:
+# with open(file_path, 'rb') as fileobj:
     # # Seek方法用于指定从第1000个字节位置开始读写。上传时会从您指定的第1000个字节位置开始上传，直到文件结束。
     # fileobj.seek(0, os.SEEK_SET)
     # # Tell方法用于返回当前位置。
     # current = fileobj.tell()
     # 填写Object完整路径。Object完整路径中不能包含Bucket名称。
-    bucket.put_object('msi', fileobj)
+    # bucket.put_object('msi/', fileobj)
+
 
 # 上传文件到OSS
 for root, dirs, files in os.walk(source_dir):
@@ -36,5 +37,8 @@ for root, dirs, files in os.walk(source_dir):
     for file in files:
         file_path = os.path.join(root, file)
         oss_path = os.path.join(target_dir, os.path.relpath(file_path, source_dir))
-        bucket.put_object_from_file(oss_path, file_path)
+        with open(file_path, 'rb') as fileobj:
+             bucket.put_object(oss_path, fileobj)
+
+        # bucket.put_object_from_file(oss_path, file_path)
         print(f"Uploaded {file_path} to OSS path {oss_path}")
