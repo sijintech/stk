bef3b22575abe6d8e30ae913c9165b2118e73529
周瀚杰 <2625553453@qq.com>
2024-01-25 23:09:32 +0800

    test

--- apps/suan/simple-cone/deploy.py
+++ apps/suan/simple-cone/deploy.py
@@ -19,33 +19,24 @@ print(f"source_dir:{source_dir}")
 
 # 必须以二进制的方式打开文件。
 # 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
-# with open(file_path, 'rb') as fileobj:
-    # # Seek方法用于指定从第1000个字节位置开始读写。上传时会从您指定的第1000个字节位置开始上传，直到文件结束。
-    # fileobj.seek(0, os.SEEK_SET)
-    # # Tell方法用于返回当前位置。
-    # current = fileobj.tell()
-    # 填写Object完整路径。Object完整路径中不能包含Bucket名称。
-    # bucket.put_object('msi/', fileobj)
 
 
-# 上传文件到OSS
-# for root, dirs, files in os.walk(source_dir):
-#     print(f"root:{root}")
-#     print(f"root:{dirs}")
-#     print(f"root:{files}")
-
-    # for file in files:
-    #     file_path = os.path.join(root, file)
-    #     oss_path = os.path.join(target_dir, os.path.relpath(file_path, source_dir))
-    #     with open(file_path, 'rb') as fileobj:
-    #          bucket.put_object(oss_path, fileobj)
-
-    #     # bucket.put_object_from_file(oss_path, file_path)
-    #     print(f"Uploaded {file_path} to OSS path {oss_path}")
 for file in os.listdir(source_dir):
-    file_path = os.path.join(source_dir, file)
-    oss_path = os.path.join(target_dir, file)
-    with open(file_path, 'rb') as fileobj:
-        bucket.put_object(oss_path, fileobj)
+    if sys.platform.startswith('linux'):
+        if file.endswith('.AppImage.tar.gz'):
+            file_path = os.path.join(source_dir, file)
+            oss_path = os.path.join(target_dir, file)
+            with open(file_path, 'rb') as fileobj:
+                bucket.put_object(oss_path, fileobj)
+            print(f"Uploaded {file_path} to OSS path {oss_path}")
+    elif sys.platform == 'win64':
+        if file.endswith('.msi.zip'):
+            file_path = os.path.join(source_dir, file)
+            oss_path = os.path.join(target_dir, file)
+            with open(file_path, 'rb') as fileobj:
+                bucket.put_object(oss_path, fileobj)
+            print(f"Uploaded {file_path} to OSS path {oss_path}")  
+    else:
+        print("Unknown operating system")
+    
 
-    print(f"Uploaded {file_path} to OSS path {oss_path}")
---
+++ apps/suan/simple-cone/update.py
@@ -0,0 +1,125 @@
+import json
+import os
+
+import oss2
+
+from oss2.credentials import EnvironmentVariableCredentialsProvider
+
+import sys
+
+
+auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
+
+
+bucket = oss2.Bucket(auth, os.environ['OSS_ENDPOINT'], os.environ['OSS_BUCKET'])
+
+
+# 填写Object完整路径，完整路径中不包含Bucket名称，例如testfolder/exampleobject.txt。
+
+# 下载Object到本地文件，并保存到指定的本地路径D:\\localpath\\examplefile.txt。如果指定的本地文件存在会覆盖，不存在则新建。
+
+this_dir=os.path.dirname(os.path.abspath(__file__))
+update_json_path=os.path.join(this_dir,"update.json")
+
+bucket.get_object_to_file('update.json', update_json_path)
+
+
+source_dir = os.path.join(this_dir, os.environ['SOURCE_DIR'])
+target_dir = os.environ['TARGET_DIR']
+oss_path=""
+
+
+# 读取 update.json
+
+with open('update.json', 'r') as f:
+
+    data = json.load(f)
+
+
+# 从环境变量中获取 tag
+
+tag = os.environ.get('TAG', '')
+
+
+# 更新 updateData 中的 name 和 pub_date
+
+data['version'] = tag
+
+data['pub_date'] = '${{ github.event.head_commit.timestamp }}'
+
+
+if sys.platform.startswith('linux'):
+
+    for file in os.listdir(source_dir):
+
+
+        if file.endswith('.AppImage.tar.gz'):
+
+            download_path="https://sijin-suan-update.oss-cn-beijing.aliyuncs.com/msi/"+file
+
+            oss_path = os.path.join(target_dir, file)
+
+            data['platforms']['linux']['url'] = download_path
+
+            data['platforms']['linux-x86_64']['url'] = download_path
+
+        if file.endswith('.AppImage.tar.gz.sig'):
+            sig_path = os.path.join(source_dir, file)
+
+            with open(sig_path, 'rb') as sig_file:
+
+                signature = sig_file.read()
+
+                data['platforms']['linux']['signature'] = signature
+
+                data['platforms']['linux-x86_64']['signature'] = signature
+    
+        
+
+elif sys.platform == 'win64':
+
+    for file in os.listdir(source_dir):
+
+
+        if file.endswith('.msi.zip'):
+
+            download_path="https://sijin-suan-update.oss-cn-beijing.aliyuncs.com/msi/"+file
+
+            oss_path = os.path.join(target_dir, file)
+
+            data['platforms']['win64']['url'] = download_path
+
+            data['platforms']['windows-x86_64']['url'] = download_path
+
+        if file.endswith('.msi.zip.sig'):
+            sig_path = os.path.join(source_dir, file)
+        
+
+            with open(sig_path, 'rb') as sig_file:
+
+                signature = sig_file.read()
+
+                data['platforms']['win64']['signature'] = signature
+
+                data['platforms']['windows-x86_64']['signature'] = signature
+    
+
+else:
+
+    print("Unknown operating system")
+
+# 将修改后的数据保存回 update.json
+
+with open(update_json_path, 'w') as f:
+
+    json.dump(data, f, indent=2)
+
+
+# 上传update.json
+
+with open(update_json_path, 'rb') as fileobj:
+
+    bucket.put_object(oss_path, fileobj)
+
+print(f"Uploaded {update_json_path} to OSS path {oss_path}")
+
